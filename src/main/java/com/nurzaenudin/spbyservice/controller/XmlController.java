5c6f56b1ccb02524e5c4b19ef92615e036303dce
nurzaenudin <nurzaenudin@SJ832PC008SW.kemenkeu.go.id>
2019-10-17 13:52:20 +0700

    sudah bisa read dari file xml

--- src/main/java/com/nurzaenudin/spbyservice/controller/XmlController.java
+++ src/main/java/com/nurzaenudin/spbyservice/controller/XmlController.java
@@ -23,27 +23,28 @@ import org.w3c.dom.NodeList;
  *
  * @author nurzaenudin
  */
-
 @Controller
 public class XmlController {
-    private final Logger logger=LoggerFactory.getLogger(XmlController.class);
-    
-    
+
+    private final Logger logger = LoggerFactory.getLogger(XmlController.class);
+
     @GetMapping("xml")
     @ResponseBody
-    public String parseSpby(){
+    public String parseSpby() {
         Spbysakti spbysakti = null;
-        
-        try{
+
+        try {
             String alamat = "Students.xml";
-            File xmlFile= new File(alamat);
+            File xmlFile = new File(alamat);
             System.out.println(xmlFile);
-            
+
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
             Document doc = builder.parse(xmlFile);
-            
+            System.out.println(doc);
+
             NodeList studentNodes = doc.getElementsByTagName("student");
+            System.out.println( "ini adalah student " + studentNodes.item(0));
             for (int i = 0; i < studentNodes.getLength(); i++) {
                 Node studentNode = studentNodes.item(i);
                 if (studentNode.getNodeType() == Node.ELEMENT_NODE) {
@@ -54,14 +55,47 @@ public class XmlController {
                     System.out.println("Student Name = " + studentName);
                 }
             }
+
+            String alamat2 = "04101.jrpxml";
+            File xmlFile2 = new File(alamat2);
+            System.out.println(xmlFile2);
+
+            DocumentBuilderFactory factory2 = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder2 = factory.newDocumentBuilder();
+            Document doc2 = builder.parse(xmlFile2);
+            System.out.println(doc2);
+
+            NodeList textNodes = doc2.getElementsByTagName("text");
+            System.out.println("ini adalah text " + textNodes); 
+            for (int i = 0; i < textNodes.getLength(); i++) {
+//                System.out.println("+++++++");
+                Node textNode = textNodes.item(i);
+                if (textNode.getNodeType() == Node.ELEMENT_NODE) {
+                    NodeList textContentNodes = textNode.getChildNodes();
+                    for (int j = 0; j < textContentNodes.getLength(); j++) {
+                        if (textContentNodes.item(j).getNodeName()=="textContent"){
+                            System.out.println(i + " : " + textContentNodes.item(j).getTextContent());
+                        }
+                        
+                       
+                        
+                    }
+                    
+//                    System.out.println("=========" + textElement.getTextContent());
+//                    String textContent = textElement.getElementsByTagName("textcontent");
+////                    String studentName = studentElement.getElementsByTagName("name").item(0).getTextContent();
+////                    System.out.println("Student Id = " + studentId);
+//                    System.out.println("Text content = " + textContent);
+                }
+            }
             
-            System.out.println(doc);
+
             
-        
-        }catch (Exception ex){
+
+        } catch (Exception ex) {
             logger.error(ex.getMessage());
         }
         return "Sukses";
-    
+
     }
 }
